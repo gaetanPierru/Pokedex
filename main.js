71c38710cea5f9fe985fe6a258e5dbb7861c9733
gaetanpierru <pierrugaetan0@gmail.com>
2023-01-31 11:13:47 +0100

    debut du projet pokedex

---
+++ main.js
@@ -0,0 +1,46 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const { autoUpdater } = require('electron-updater');
+const path = require('path')
+
+
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+      }
+    })
+    win.loadFile('index.html')
+
+    win.once('ready-to-show', () => {
+      autoUpdater.checkForUpdatesAndNotify();
+    })
+  
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
+
+
+
+  ipcMain.on('app_version', (event) => {
+    event.sender.send('app_version', { version: app.getVersion() });
+  });
+
+  autoUpdater.on('update-available', () => {
+    win.webContents.send('update_available');
+  });
+  autoUpdater.on('update-downloaded', () => {
+    win.webContents.send('update_downloaded');
+  });
