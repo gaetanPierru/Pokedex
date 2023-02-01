1b7ac2325167b77e06d6ac63e8ba217980dced1e
gaetanpierru <pierrugaetan0@gmail.com>
2023-02-01 09:55:32 +0100

    icons and updated work ?

--- main.js
+++ main.js
@@ -1,50 +1,84 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const { autoUpdater } = require('electron-updater');
 const path = require('path')
-require('dotenv')
+const sqlite = require('sqlite-electron')
 
+let win
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      webPreferences: {
-        preload: path.join(__dirname, 'preload.js')
-      }
-    })
-    win.loadFile('index.html')
+   win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: __dirname + '/images/pokedex.icns',
+    webPreferences: {
+      nodeIntegration: true,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  win.loadFile('index.html')
 
-    win.once('ready-to-show', () => {
-      autoUpdater.checkForUpdatesAndNotify();
-    })
-  
+  win.once('ready-to-show', () => {
+    autoUpdater.checkForUpdatesAndNotify();
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+ipcMain.on('restart_app', () => {
+  autoUpdater.quitAndInstall();
+});
+
+autoUpdater.on('update-available', () => {
+  win.webContents.send('update_available');
+});
+autoUpdater.on('update-downloaded', () => {
+  win.webContents.send('update_downloaded');
+});
+
+ipcMain.handle('createInMemoryDatabase', async() => {
+  try {
+    return await sqlite.setdbPath(':memory:')
+  } catch (error) {
+    return error
   }
+})
 
-  app.whenReady().then(() => {
-    createWindow()
+ipcMain.handle("executeQuery", async (event, query, fetch, value) => {
+  try {
+    return await executeQuery(query, fetch, value);
+  } catch (error) {
+    return error;
+  }
+});
 
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
+ipcMain.handle("executeMany", async (event, query, values) => {
+  try {
+    return await executeMany(query, values);
+  } catch (error) {
+    return error;
+  }
+});
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit()
-  })
-
-
-
-  ipcMain.on('app_version', (event) => {
-    event.sender.send('app_version', { version: app.getVersion() });
-  });
-
-  ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
-  });
-
-  autoUpdater.on('update-available', () => {
-    win.webContents.send('update_available');
-  });
-  autoUpdater.on('update-downloaded', () => {
-    win.webContents.send('update_downloaded');
-  });
+ipcMain.handle('executeScript', async (event, scriptpath) => {
+  try {
+    return await sqlite.executeScript('CREATE TABLE IF NOT EXISTS pokedex (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,NOM TEXT NOT NULL,SHINY INTEGER NOT NULL DEFAULT 0, COMPTEUR INTEGER NOT NULL DEFAULT 0);');
+  } catch (error) {
+    return error
+  }
+})
--- preload.js
+++ preload.js
@@ -1,10 +1,53 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-  
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency])
-    }
-  })
+const { contextBridge, ipcRenderer } = require('electron')
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+
+  const version = document.getElementById("version");
+
+  const notification = document.getElementById("notification");
+  const message = document.getElementById("message");
+  const restartButton = document.getElementById("restart-button");
+
+  ipcRenderer.on("update_available", () => {
+    ipcRenderer.removeAllListeners("update_available");
+    message.innerText = "A new update is available. Downloading now...";
+    notification.classList.remove("hidden");
+  });
+
+  ipcRenderer.on("update_downloaded", () => {
+    ipcRenderer.removeAllListeners("update_downloaded");
+    message.innerText =
+      "Update Downloaded. It will be installed on restart. Restart now?";
+    restartButton.classList.remove("hidden");
+    notification.classList.remove("hidden");
+  });
+
+  ipcRenderer.send("app_version");
+  ipcRenderer.on("app_version", (event, arg) => {
+    ipcRenderer.removeAllListeners("app_version");
+    console.log(arg);
+    version.innerText = "Version " + arg.version;
+  });
+})
+
+contextBridge.exposeInMainWorld('api', {
+  closeNotification : () => {
+    notification.classList.add("hidden");
+  },
+  restartApp : () => {
+    ipcRenderer.send("restart_app");
+  }
+})
+
+
+/*
+
+    */
